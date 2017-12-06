04c3ec111bdcad5bf038d230ac436c60678dcc9a
Gourav Shah <gs@initcron.org>
2017-12-06 11:01:04 +0530

    updated chef solo configs

--- solo.rb
+++ solo.rb
@@ -1,4 +1,1 @@
-cookbook_path /opt/sample-chef-cookbooks
-log_level :info
-log_location /var/log/chef-solo.log
-json_attribs /opt/sample-chef-cookbooks/node.json
+cookbook_path "/opt/sample-chef-cookbooks"
